a1436b3a0b1660a6043a08c4f95eb9dbf974c34b
João Paulo <jpfricks@gmail.com>
2020-10-14 00:52:07 -0300

    chore(backend): configure eslint, prettier and editor config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,25 @@
-import express from 'express'; 
+import express from 'express';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response) => {
-  return response.json({ message: 'Olá mundo' });
-});
+function logRequests(request, response, next) {
+  const { method, url } = request;
 
-app.listen(3333);
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+
+  return next();
+}
+
+app.use(logRequests);
+
+app.get('/users', (request, response) => response.json({ message: 'Olá mundo' }));
+
+app.listen(3333, () => {
+  console.log('⚙️  Server started on port 3333!');
+});
