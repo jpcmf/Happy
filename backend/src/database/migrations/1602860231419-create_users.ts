bb05df497ac1b31647cf60ae0cbf36cb2ab0dffe
João Paulo <jpfricks@gmail.com>
2020-10-22 15:57:03 -0300

    chore(backend): create the relation ManyToOne with User

--- backend/src/database/migrations/1602860231419-create_users.ts
+++ backend/src/database/migrations/1602860231419-create_users.ts
@@ -8,7 +8,7 @@ export default class createUsers1602860231419 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
