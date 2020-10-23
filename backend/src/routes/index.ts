04760a86410bffcf5bf2384a3b2bb334c689bf5e
João Paulo <jpfricks@gmail.com>
2020-10-23 15:54:39 -0300

    feature(users): create user registration and adjust create orphanage method

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import uploadConfig from '../config/upload';
+import usersRouter from './users.routes';
 import orphanagesRouter from './orphanages.routes';
 
 const routes = Router();
 const upload = multer(uploadConfig);
 
 routes.use('/orphanages', upload.array('images'), orphanagesRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
