31f5c2b831effd0b918b397f0a9d97a71d47b8f0
João Paulo <jpfricks@gmail.com>
2020-10-26 19:11:39 -0300

    chore(backend): update the user avatar

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,9 +1,13 @@
 import multer from 'multer';
 import path from 'path';
 
+const uploadFolder = path.join(__dirname, '..', '..', 'uploads');
+
 export default {
+  directory: uploadFolder,
+
   storage: multer.diskStorage({
-    destination: path.join(__dirname, '..', '..', 'uploads'),
+    destination: uploadFolder,
     filename: (request, file, cb) => {
       const fileName = `${Date.now()}-${file.originalname}`;
 
