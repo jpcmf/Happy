a4ab9cf9aee01c2f985c502536f700c3f2db8416
João Paulo Fricks <jpfricks@gmail.com>
2020-11-17 00:13:57 -0300

    feature(frontend): create sign in page and improvements of ui - wip

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 
 import { Container } from './styles';
 
+import { Sidebar, Input } from '../../components';
+
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   return (
     <Container>
-      <h1>SignIn</h1>
+      <Sidebar />
+
+      <main>
+        <Form
+          onSubmit={() => {}}
+          ref={formRef}
+          className="create-orphanage-form"
+        >
+          <fieldset>
+            <legend>Login</legend>
+          </fieldset>
+        </Form>
+      </main>
     </Container>
   );
 };
--- frontend/src/pages/SignIn/styles.ts
+++ frontend/src/pages/SignIn/styles.ts
@@ -1,3 +1,43 @@
 import styled from 'styled-components';
+import colors from '../../styles/colors';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  height: 100vh;
+
+  main {
+    flex: 1;
+  }
+
+  form.create-orphanage-form {
+    animation: 500ms ease-out 0s 1 slideInFromUp;
+    background: #ffffff;
+    border-radius: 20px;
+    border: 1px solid #d3e2e5;
+    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+      0 4px 6px -2px rgba(0, 0, 0, 0.05);
+    margin: 64px auto;
+    overflow: hidden;
+    padding: 64px 80px;
+    width: 700px;
+
+    fieldset {
+      border: 0;
+
+      + fieldset {
+        margin-top: 80px;
+      }
+
+      legend {
+        width: 100%;
+        font-size: 32px;
+        line-height: 34px;
+        color: #5c8599;
+        font-weight: 700;
+        border-bottom: 1px solid #d3e2e5;
+        margin-bottom: 40px;
+        padding-bottom: 24px;
+      }
+    }
+  }
+`;
