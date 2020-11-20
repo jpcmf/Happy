bb5010fe6a669c220b036cbb42495af7d2a7e272
João Paulo Fricks <jpfricks@gmail.com>
2020-11-20 13:49:10 -0300

    feature(storybook): WIP

---
+++ frontend/.storybook/main.js
@@ -0,0 +1,10 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  stories: ['../src/*/.stories.@(tsx|jsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
+    return config;
+  },
+};
