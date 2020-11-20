804cacccc78d7914b47155cae8476a13a4123dfe
Andre Morpa <andremorpa@gmail.com>
2020-11-20 21:54:46 +0000

    Add Storybook in project

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,10 +1,8 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
 module.exports = {
-  stories: ['../src/*/.stories.@(tsx|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: async (config, { configType }) => {
-    config.resolve.plugins = [new TsconfigPathsPlugin()];
-    return config;
-  },
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+  ],
 };
